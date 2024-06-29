b3a6f8fad5b7ac3900ca476682f84751d21a2212
Bhargav Parsi <bhargav265@gmail.com>
2024-06-29 16:21:30 -0700

    Update EmptyEditor.tsx

--- src/client/components/Editor/EmptyEditor.tsx
+++ src/client/components/Editor/EmptyEditor.tsx
@@ -4,8 +4,11 @@ export const EmptyEditor: React.FC = () => {
   return (
     <div className="empty-editor v-center" data-testid="empty-editor">
       <div className="text-center">
-        <p>
-          <kbd>CTRL</kbd> + <kbd>ALT</kbd> + <kbd>N</kbd>
+                <p>
+          <kbd>Adding</kbd> + <kbd>this</kbd> + <kbd>N</kbd>
+        </p>
+                <p>
+          <kbd>else</kbd> + <kbd>something</kbd> + <kbd>N</kbd>
         </p>
       </div>
     </div>
